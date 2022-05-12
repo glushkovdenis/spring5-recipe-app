168aa4348bee8350545c66616b353bfe3a92abfd
Hieronymus <erickaufman1961@gmail.com>
2022-05-12 21:14:25 +0300

    Adding static resources. Closes #17

---
+++ src/main/java/com/glushkovdenis/spring5recipeapp/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.glushkovdenis.spring5recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        return "index";
+    }
+}
